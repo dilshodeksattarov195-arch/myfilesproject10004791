3d324b41e62bdab21f3cd038f6615d078f79b764
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-29 17:17:33 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class filterVarseWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = filterVarseWorker()
+    print("Worker engine initialized.")
